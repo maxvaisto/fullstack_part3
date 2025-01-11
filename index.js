07748a0ab1cdef04caf1fa8d865d31654bea1043
Max Väistö <max.vaisto@gmail.com>
2025-01-11 13:32:15 +0200

    Added part3 a

---
+++ index.js
@@ -0,0 +1,106 @@
+const express = require('express')
+const app = express()
+var morgan = require('morgan')
+
+app.use(express.json())
+
+app.use(morgan(function (tokens, req, res) {
+  if (req.method === "POST") {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      // response time
+      tokens['response-time'](req, res), 'ms',
+      JSON.stringify(req.body),
+    ].join(' ');
+  }
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    // response time
+    tokens['response-time'](req, res), 'ms',
+  ].join(' ');
+}));
+
+
+let phonebook = [
+    {
+      "id": "1",
+      "name": "Arto Hellas",
+      "number": "040-123456"
+    },
+    {
+      "id": "2",
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523"
+    },
+    {
+      "id": "3",
+      "name": "Dan Abramov",
+      "number": "12-43-234345"
+    },
+    {
+      "id": "4",
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(phonebook)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const person = phonebook.find(person => person.id === request.params.id)
+  if (person) {
+    response.json(person)
+  }
+  else {
+    response.status(404).end()
+  }
+})
+
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+
+  if (phonebook.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 1000),
+    name: body.name,
+    number: body.number
+  }
+  phonebook = phonebook.concat(person)
+  response.json(person)
+})
+
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${phonebook.length} people</p>
+                          <p>${Date()}</p>`)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
