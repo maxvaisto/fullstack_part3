29fe3be1c01cdf6ed94fa61d01045c362b7cc428
Max Väistö <max.vaisto@gmail.com>
2025-01-18 15:14:02 +0200

    Added cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 
 app.use(morgan(function (tokens, req, res) {
@@ -100,7 +102,7 @@ app.get('/info', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT ||  3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
