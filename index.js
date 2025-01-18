b24bbca8923dcd4c6eb1fbae80ad0b900a11a849
Max Väistö <max.vaisto@gmail.com>
2025-01-19 00:08:45 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
+const mongoose = require('mongoose')
+
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
+
 
 app.use(morgan(function (tokens, req, res) {
   if (req.method === "POST") {
@@ -30,45 +35,33 @@ app.use(morgan(function (tokens, req, res) {
 }));
 
 
-let phonebook = [
-    {
-      "id": "1",
-      "name": "Arto Hellas",
-      "number": "040-123456"
-    },
-    {
-      "id": "2",
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523"
-    },
-    {
-      "id": "3",
-      "name": "Dan Abramov",
-      "number": "12-43-234345"
-    },
-    {
-      "id": "4",
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (request, response) => {
-  response.json(phonebook)
-})
 
 app.get('/api/persons/:id', (request, response) => {
-  const person = phonebook.find(person => person.id === request.params.id)
-  if (person) {
-    response.json(person)
-  }
-  else {
-    response.status(404).end()
-  }
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    }
+  })
+    .catch(
+      error => {
+        console.log(error)
+        response.status(404).end()
+      }
+    )
+
+
+})
+
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(result => {
+    response.json(result)
+  })
+
 })
 
 
@@ -82,33 +75,46 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if (phonebook.find(person => person.name === body.name)) {
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
-  }
+  Person.find( {name: body.name}).then(result =>
+    {
+      if (result.length > 0) {
+        return response.status(400).json({
+          error: 'name must be unique'
+        })
+      }
+      else {
+        const person = new Person({
+          id: '' + Math.floor(Math.random() * 1000),
+          name: body.name,
+          number: body.number
+        })
 
-  const person = {
-    id: '' + Math.floor(Math.random() * 1000),
-    name: body.name,
-    number: body.number
-  }
-  phonebook = phonebook.concat(person)
-  response.json(person)
+        person.save().then(savedPerson => {
+          response.json(savedPerson)
+        })
+      }
+    }
+  )
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  phonebook = phonebook.filter(person => person.id !== request.params.id)
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id).then(result => {
+    response.status(204).end()
+  })
+    .catch(error => {
+      console.log(error)
+      response.status(404).end()
+    })
 })
 
 app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${phonebook.length} people</p>
-                          <p>${Date()}</p>`)
+  Person.find({}).then(result => {
+    response.send(`<p>Phonebook has info for ${result.length} people</p><p>${Date()}</p>`)
+  })
 })
 
 
-const PORT = process.env.PORT ||  3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
